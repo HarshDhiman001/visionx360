098fc39e8de4429d1391ea285654dcfa4241779e
Harsh Dhiman <harshdhiman@Harshs-MacBook-Air.local>
2025-09-18 06:34:59 +0530

    Update homepage/about section

--- src/Team.jsx
+++ src/Team.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ceoImg from "./assets/ceo.jpeg";
+import ceo from "./assets/ceo.jpeg";
 
 /*
   Usage:
@@ -9,7 +9,7 @@ import ceoImg from "./assets/ceo.jpeg";
 
 const members = [
   { name: "Harsh D.", role: "Founder - CEO", img: "/ceo.jpeg" },
-  { name: "Preeti D.", role: "Co-Founder - COO", img: {ceoImg} },
+  { name: "Preeti D.", role: "Co-Founder - COO", img: {ceo} },
   { name: "Guy Hawkins", role: "Executive Admin", img: "https://i.pinimg.com/1200x/49/22/3c/49223c4160a61dadc6556851bcc39c32.jpg" },
   { name: "Cody Fisher", role: "Office Manager", img: "https://i.pinimg.com/1200x/49/22/3c/49223c4160a61dadc6556851bcc39c32.jpg" },
 ];
